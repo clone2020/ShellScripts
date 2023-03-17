73e3f9bb79de46272b3492f7649109cfd36478df
Raja shekhar <goodwill5585@gmail.com>
2023-03-17 06:42:04 +0530

    modified the dry-run condition

--- aws_ami_snap_deregister_delete.bash
+++ aws_ami_snap_deregister_delete.bash
@@ -11,25 +11,20 @@ echo -n "dryrun:"
 read dryrun
 
 declare amis="$(aws ec2 describe-images --region=${region} --filters "Name=tag:Name,Values=${app}*" --query "Images[?CreationDate<=\`$date\`] | sort_by(@,&CreationDate)[].{id:ImageId}" --output text
-declare ami_last="$(echo "${amis[*]}" | tail -n 1)"
+#aws ec2 describe-images --filters "Name=tag:Name,Values=${app}*" --query "Images[?CreationDate<=\`$date\`] | sort_by(@,&CreationDate)[].{id:ImageId,date:CreationDate}" --output text
 
 if[ ! -z "$amis" ]
 then
-
        for ami in ${amis[@]}
        do
-
                declare snap="$(aws ec2 describe-images --image-id $ami --query 'Images[*].[BlockDeviceMappings[*].Ebs.SnapshotId]' --output text)"
+               declare createdate="$(aws ec2 describe-images --image-id $ami --query 'Images[*].[CreationDate]' --output text)"
 
                if [ ! -z "$snap" ]
                then
                        if [ $dryrun == 'true' ]
                        then
-                               if [ $ami == "${ami_last}" ]
-                               then
-                                       aws ec2 describe-images --filters "Name=tag:Name,Values=${app}*" --query "Images[?CreationDate<=\`$date\`] | sort_by(@,&CreationDate)[].{id:ImageId,date:CreationDate}" --output text
-                                       echo "This is a dry-run"
-                               fi
+                               echo "Dry-run for AMI:$ami $createdate and Snapshot:$snap - no action taken"
                        else
                                aws ec2 deregister-image --image-id $ami
                                aws ec2 delete-snapshot --snapshot-id $snap
@@ -38,3 +33,7 @@ then
                 fi
         done
 fi
+
+echo ;
+echo "Done";
+echo ;
